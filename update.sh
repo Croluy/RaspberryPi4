8f6f9e515b51ec71344ae1141e748136f835eae3
Croluy <croluy@fbi.ac>
2023-09-05 20:49:45 +0200

    added pi script

--- update.sh
+++ update.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-source ../../.env/.env
-
-echo "Updating Raspbot package..."
+source ../../.env/.env;
+echo "Updating Raspbot package...";
+npm uninstall raspbot;
 npm i raspbot;
-echo "Raspbot package updated!";
+echo "Raspbot package updated!\n";
 echo "Copio il file di configurazione...";
-cp ../../.env/.env .env
-echo "File di configurazione copiato!";
+cp ../../.env/.env .env;
+echo "File di configurazione copiato!\n";
 echo "Restarting daemon...";
 echo $PI_PASS | sudo -S systemctl daemon-reload
-echo "Daemons reloaded!";
+echo "Daemons reloaded!\n";
 echo "Restarting service...";
 echo $PI_PASS | sudo -S systemctl restart telegram_bot.service;
-echo "Service restarted!";
-echo "Raspbot updated and restarted!";
+echo "Service restarted!\n";
+echo "✅ Raspbot updated and restarted!";
